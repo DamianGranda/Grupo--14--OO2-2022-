66e72a2b559418991962d50782d04e55fd623233
Mauro Bazante <67117511+maubazante@users.noreply.github.com>
2022-05-26 16:34:30 -0300

    Prueba Github vs Sourcetree
    -- Prueba

---
+++ HomeController.java
@@ -0,0 +1,13 @@
+package sistema.integrador.oo2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "Por favor mostrá mi nombre";
+	}
+}
