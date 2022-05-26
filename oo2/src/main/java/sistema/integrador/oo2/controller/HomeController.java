9dc44e19d8a37e4be80fc97d9bb17b3b26366e92
maubazante <maurobazante3@gmail.com>
2022-05-26 19:14:14 -0300

    -- Conexión hecha con base de datos. -- Avances con el template

--- oo2/src/main/java/sistema/integrador/oo2/controller/HomeController.java
+++ oo2/src/main/java/sistema/integrador/oo2/controller/HomeController.java
@@ -1,12 +1,13 @@
 package sistema.integrador.oo2.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 	
-	@GetMapping("/")
+	@RequestMapping({"/index", "/home", "/"})
 	public String index() {
 		return "home";
 	}
